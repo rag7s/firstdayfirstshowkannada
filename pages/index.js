a461e347ccdf30512b88a91f644d682e648a9ff9
Raghavendra S <raghavendra.sb70@gmail.com>
2022-01-30 23:57:03 +0530

    Welcome message

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>First Day First Show Kannada</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my First Day First Show Kannada Website" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Under Construction!!
         </p>
       </main>
 
